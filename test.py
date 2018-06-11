9ca9d91dc055402c559a1716a11516c2081b281a
LangJin <fenyukuang@gmail.com>
2018-06-12 01:18:33 +0800

    add requests

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # -*- conding:utf-8 -*-
 import unittest
 import HTMLTestRunner
+import requests
 
 class TestDemo(unittest.TestCase):
 
@@ -27,10 +28,20 @@ class TestDemo(unittest.TestCase):
         b = 6
         self.assertEqual(a*b,5)
 
+    def test_login(self):
+        url = "http://127.0.0.1:8808/login"
+        json = {"username":"admin","password":"123456"}
+        headers = {
+            'Content-Type': "application/json"
+            }
+        response = requests.request("POST", url, json=json, headers=headers)
+        validata = response.json()
+        code = validata.get("code")
+        self.assertEqual(code,200)
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    tests = [TestDemo("test_01"),TestDemo("test_02"),TestDemo("test_03")]
+    tests = [TestDemo("test_01"),TestDemo("test_02"),TestDemo("test_03"),TestDemo("test_login")]
     suite.addTests(tests)
     with open("测试报告.html","wb") as f:
         runner = HTMLTestRunner.HTMLTestRunner(stream=f,verbosity=2)
